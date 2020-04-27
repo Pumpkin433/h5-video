27b170b3b46094d84c1fd0fc5096caec3cc30468
xuliulei <xuliulei666@163.com>
2020-04-27 23:51:48 +0800

    night

---
+++ pages/sports/mid.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:'mid',
+		onShow(){
+			uni.reLaunch({
+				url:'/'
+			})
+		},
+		onLoad(){
+			
+		}
+	}
+</script>
+
+<style>
+</style>
