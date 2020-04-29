9a71199a8ed21c241d7b9d842571d4b802af5b90
xuliulei <xuliulei666@163.com>
2020-04-29 14:36:15 +0800

    修改pages.json

---
+++ unpackage/dist/build/mp-weixin/components/page-head.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/page-head"],{5170:function(t,e,n){"use strict";n.r(e);var u=n("d723"),r=n("76b7");for(var a in r)"default"!==a&&function(t){n.d(e,t,(function(){return r[t]}))}(a);var c,f=n("f0c5"),o=Object(f["a"])(r["default"],u["b"],u["c"],!1,null,null,null,!1,u["a"],c);e["default"]=o.exports},"76b7":function(t,e,n){"use strict";n.r(e);var u=n("8ceb"),r=n.n(u);for(var a in u)"default"!==a&&function(t){n.d(e,t,(function(){return u[t]}))}(a);e["default"]=r.a},"8ceb":function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;var u={name:"page-head",props:{title:{type:String,default:""}}};e.default=u},d723:function(t,e,n){"use strict";var u,r=function(){var t=this,e=t.$createElement;t._self._c},a=[];n.d(e,"b",(function(){return r})),n.d(e,"c",(function(){return a})),n.d(e,"a",(function(){return u}))}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/page-head-create-component',
+    {
+        'components/page-head-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("5170"))
+        })
+    },
+    [['components/page-head-create-component']]
+]);
