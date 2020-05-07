1dc876a3685a15694259bdc572ea0368a3362ab4
xuliulei <xuliulei666@163.com>
2020-05-07 19:09:23 +0800

    5.7

---
+++ pages/index/index.vue
@@ -0,0 +1,453 @@
+<template>
+	<view class="uni-flex uni-column index-bg">
+		<view class="flex-item flex-item-V index-banner">
+			<view class="flex-item flex-item-V index-info">
+				<view class="uni-flex uni-row index-info-item">
+					<view class="flex-item index-info-1">
+						每日阅读
+						<span>3</span>
+						篇文章 30积分
+					</view>
+					<view class="flex-item index-info-2">3/3</view>
+					<view class="flex-item index-info-3"><button>领取积分</button></view>
+				</view>
+				<view class="uni-flex uni-row index-info-item">
+					<view class="flex-item index-info-1">
+						每日阅读
+						<span>3</span>
+						篇讨论帖 30积分
+					</view>
+					<view class="flex-item index-info-2">3/3</view>
+					<view class="flex-item index-info-3"><button>领取积分</button></view>
+				</view>
+
+				<view class="uni-flex uni-row index-info-item">
+					<view class="flex-item index-info-1">
+						在任意文章/讨论帖留言
+						<span>3</span>
+						次 30积分
+					</view>
+					<view class="flex-item index-info-2">3/3</view>
+					<view class="flex-item index-info-3"><button>领取积分</button></view>
+				</view>
+
+				<view class="uni-flex uni-row index-info-item">
+					<view class="flex-item index-info-1">
+						分享
+						<span>1</span>
+						篇文章/讨论帖 10积分
+					</view>
+					<view class="flex-item index-info-2">0/1</view>
+					<view class="flex-item index-info-3"><button>领取积分</button></view>
+				</view>
+			</view>
+		</view>
+
+		<view class="uni-flex uni-row index-my-score">
+			<view class="flex-item index-my-score-l">我的积分：999,999,999分</view>
+			<view class="flex-item index-my-score-r">兑换记录</view>
+		</view>
+
+		<view class=" prize-list">
+			<view class="score-store-txt">积分商城</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1"><img src="https://aloss.hotforest.cn/sign/s1.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s2"><img src="https://aloss.hotforest.cn/sign/s2.png" alt="s1" /></view>
+				<view class="score-store-item-2">雷蛇皮卡丘限定联名鼠标</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">9000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s3"><img src="https://aloss.hotforest.cn/sign/s3.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+			<view class="score-store-item">
+				<view class="score-store-item-1 s4"><img src="https://aloss.hotforest.cn/sign/s4.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s5"><img src="https://aloss.hotforest.cn/sign/s5.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s6"><img src="https://aloss.hotforest.cn/sign/s6.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s7"><img src="https://aloss.hotforest.cn/sign/s7.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s8"><img src="https://aloss.hotforest.cn/sign/s8.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s9"><img src="https://aloss.hotforest.cn/sign/s9.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+
+			<view class="score-store-item">
+				<view class="score-store-item-1 s10"><img src="https://aloss.hotforest.cn/sign/s10.png" alt="s1" /></view>
+				<view class="score-store-item-2">苹果 iphone 11</view>
+				<view class="score-store-item-3">
+					<view class="score-store-item-3-l">10000 积分</view>
+					<view class="score-store-item-3-r">我要兑换</view>
+				</view>
+			</view>
+		</view>
+
+		<view class="uni-flex uni-column index-profile-bg">
+			<view class="flex-item flex-item-V index-profile-text">提现说明</view>
+			<view class="flex-item flex-item-V index-profile-1">1，活动时间：5月6日~9月6日</view>
+			<view class="flex-item flex-item-V index-profile-2">2，用户可以通过完成任务获取每日积分，积分可以在积分商城内兑换商品，</view>
+			<view class="flex-item flex-item-V index-profile-3">3，需要兑换奖品的用户，请添加下方客服微信，奖品将由客服为您发放； 奖品兑换时间：每月1号~10号</view>
+			<view class="flex-item flex-item-V index-profile-4">
+				<view><img src="https://aloss.hotforest.cn/sign/wxcode.png" alt="wxcode" /></view>
+				<view class="index-profile-4-text">
+					微信号:NationalSports
+					<span @click="copy">复制号码</span>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+// 引入文件
+import h5Copy from '@/js_sdk/junyi-h5-copy/junyi-h5-copy/junyi-h5-copy.js';
+
+export default {
+	name: 'index',
+	data() {
+		return {};
+	},
+	onLoad(option) {},
+	methods: {
+		// 触发方法
+		copy() {
+			let content = 'NationalSports'; // 复制内容，必须字符串，数字需要转换为字符串
+			const result = h5Copy(content);
+			if (result === false) {
+				uni.showToast({
+					title: '不支持'
+				});
+			} else {
+				uni.showToast({
+					title: '复制成功',
+					icon: 'none'
+				});
+			}
+		}
+	}
+};
+</script>
+
+<style>
+.index-bg {
+	background: url(https://aloss.hotforest.cn/sign/index-bg.png) no-repeat center;
+	background-size: 100% 110%;
+	width: 100%;
+	height: 2882rpx;
+}
+.index-banner {
+	width: 752rpx;
+	height: 870rpx;
+	margin: 0 auto;
+	background: url(https://aloss.hotforest.cn/sign/index-banner.png) no-repeat center;
+	background-size: 100% 100%;
+	text-align: center;
+}
+
+.index-info {
+	width: 658rpx;
+	height: 456rpx;
+	margin: 0 auto;
+	margin-top: 520rpx;
+}
+.index-info-item {
+	width: 617rpx;
+	height: 85rpx;
+	margin: 0 auto;
+	border-bottom: 1rpx dashed rgba(253, 110, 105, 1);
+}
+
+.index-info-1 {
+	width: 60%;
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 85rpx;
+	text-align: left;
+}
+.index-info-1 span {
+	color: #fd6e69;
+	font-size: 30rpx;
+}
+.index-info-2 {
+	width: 20%;
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 85rpx;
+}
+.index-info-3 {
+	width: 20%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.index-info-3 button {
+	width: 98rpx;
+	height: 39rpx;
+	outline: none;
+	border: 1rpx solid transparent;
+	background: url(https://aloss.hotforest.cn/sign/button.png) no-repeat center;
+	background-size: 100% 100%;
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 39rpx;
+}
+
+.index-my-score {
+	margin-top: 35rpx;
+	padding-left: 50rpx;
+	padding-right: 47rpx;
+	margin-bottom: 35rpx;
+}
+.index-my-score-l {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 35rpx;
+	width: 50%;
+	text-align: left;
+}
+.index-my-score-r {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 35rpx;
+	width: 50%;
+	text-align: right;
+}
+
+.prize-list {
+	width: 659rpx;
+	height: 1367rpx;
+	margin: 0 auto;
+	background: url(https://aloss.hotforest.cn/sign/prize-list-bg.png) no-repeat center;
+	background-size: 100% 100%;
+}
+
+.score-store-txt {
+	text-align: center;
+	font-size: 24rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	position: absolute;
+	left: 330rpx;
+}
+.score-store-item {
+	width: 50%;
+	height: 269rpx;
+	text-align: center;
+	float: left;
+}
+.score-store-item-1 {
+	width: 100%;
+	height: 130rpx;
+	padding-top: 40rpx;
+}
+.score-store-item-1 img {
+	width: 74rpx;
+	height: 101rpx;
+}
+
+.score-store-item-2 {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 35rpx;
+}
+
+.score-store-item-3 {
+	width: 80%;
+	margin: 0 auto;
+}
+.score-store-item-3-l {
+	width: 50%;
+	float: left;
+
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 35rpx;
+}
+.score-store-item-3-r {
+	width: 50%;
+	float: right;
+
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	line-height: 35rpx;
+
+	background: rgba(253, 109, 101, 1);
+	border-radius: 17px;
+}
+.s2 img {
+	width: 162rpx;
+	height: 117rpx;
+}
+.s3 img {
+	width: 77rpx;
+	height: 96rpx;
+}
+.s4 img {
+	width: 122rpx;
+	height: 95rpx;
+}
+.s5 img {
+	width: 156rpx;
+	height: 124rpx;
+}
+.s6 img {
+	width: 206rpx;
+	height: 169rpx;
+}
+.s7 img {
+	width: 196rpx;
+	height: 99rpx;
+}
+.s8 img {
+	width: 137rpx;
+	height: 87rpx;
+}
+.s9 img {
+	width: 133rpx;
+	height: 121rpx;
+}
+.s10 img {
+	width: 100rpx;
+	height: 95rpx;
+}
+
+.index-profile-bg {
+	width: 594rpx;
+	height: 461rpx;
+	margin: 0 auto;
+	margin-top: 20rpx;
+	padding-left: 26rpx;
+	padding-right: 40rpx;
+	background: url(https://aloss.hotforest.cn/sign/index-profile-bg.png) no-repeat center;
+	background-size: 100% 100%;
+}
+.index-profile-text {
+	text-align: center;
+	font-size: 24rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(255, 255, 255, 1);
+	/* line-height:62rpx; */
+}
+.index-profile-1 {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 35rpx;
+	margin-top: 33rpx;
+}
+.index-profile-2 {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 35rpx;
+}
+.index-profile-3 {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 35rpx;
+}
+
+.index-profile-4 {
+	text-align: center;
+}
+.index-profile-4-text {
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(51, 51, 51, 1);
+	line-height: 35rpx;
+}
+.index-profile-4 img {
+	width: 142rpx;
+	height: 143rpx;
+}
+.index-profile-4 span {
+	margin-left: 30rpx;
+	font-size: 18rpx;
+	font-family: Lantinghei SC;
+	font-weight: 600;
+	color: rgba(67, 147, 255, 1);
+	line-height: 35rpx;
+}
+</style>
